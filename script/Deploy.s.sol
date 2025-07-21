c3eb149f14cbcbd90919df3be2688eef4c78123b
Sebastian T F <sebastian.tharakan97@gmail.com>
2025-07-21 21:46:43 +0530

    fix: simplify deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,58 +11,24 @@ contract DeployScript is Script {
 
     function run() public {
         address deployer = msg.sender;
-        address create2Deployer = 0x4e59b44847b379578588920cA78FbF26c0B4956C; // foundry uses this contract by default
         uint256 salt = 9999999999999999999999; // change this before deploying if needed
         console.log("Deployer: ", deployer);
-        console.log("create2Deployer: ", create2Deployer);
         console.log("Salt Seed: ", salt);
 
-        bungeeApproveAndBridge = BungeeApproveAndBridge(deploy(salt, create2Deployer));
+        bungeeApproveAndBridge = BungeeApproveAndBridge(deploy(salt));
         console.log("Deployed BungeeApproveAndBridge at: ", address(bungeeApproveAndBridge));
     }
 
-    function deploy(uint256 salt, address deployer) public returns (address addr) {
-        bytes memory initCode = getInitCode();
-        address computedAddress = computeCreate2Address(salt, deployer, initCode);
-        console.log("computedAddress: %s", computedAddress);
-
+    function deploy(uint256 salt) public returns (address addr) {
         bool ENABLE_TXNS = vm.envBool("ENABLE_TXNS");
         if (!ENABLE_TXNS) {
             console.log("Skipping deployment of contract due to ENABLE_TXNS flag");
-            return computedAddress;
         }
-
-        if (contractDeployed(computedAddress)) {
-            console.log("Contract already deployed at: %s", computedAddress);
-            return computedAddress;
-        }
-        console.log("Deploying contract... %s", computedAddress);
 
         BungeeApproveAndBridge _bungeeApproveAndBridge = new BungeeApproveAndBridge{salt: bytes32(salt)}(SOCKET_GATEWAY);
         addr = address(_bungeeApproveAndBridge);
-
         console.log("Deployed contract to: %s", address(addr));
-        require(addr == computedAddress, "Contract address mismatch due to mismatched deployer.");
 
         return addr;
-    }
-
-    function getInitCode() public pure returns (bytes memory) {
-        bytes memory initCode = abi.encodePacked(type(BungeeApproveAndBridge).creationCode, abi.encode(SOCKET_GATEWAY));
-        return initCode;
-    }
-
-    function computeCreate2Address(uint256 salt, address deployer, bytes memory initCode)
-        public
-        pure
-        returns (address)
-    {
-        bytes32 _hash = keccak256(abi.encodePacked(bytes1(0xff), deployer, salt, keccak256(initCode)));
-        address computedAddress = address(uint160(uint256(_hash)));
-        return computedAddress;
-    }
-
-    function contractDeployed(address addr) public view returns (bool) {
-        return addr.code.length > 0;
     }
 }
