fb929700c0139a3bd930656314128ec9c3f46aeb
Sebastian T F <sebastian.tharakan97@gmail.com>
2025-07-21 22:15:42 +0530

    chore: compute create2 address

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,18 +10,27 @@ contract DeployScript is Script {
     address constant SOCKET_GATEWAY = 0x3a23F943181408EAC424116Af7b7790c94Cb97a5;
 
     function run() public {
-        address deployer = msg.sender;
-        uint256 salt = 9999999999999999999999; // change this before deploying if needed
-        console.log("Deployer: ", deployer);
-        console.log("Salt Seed: ", salt);
+        address txOrigin = msg.sender;
+        address create2Deployer = 0x4e59b44847b379578588920cA78FbF26c0B4956C; // foundry uses this contract by default
+        bytes32 salt = keccak256(abi.encode(uint256(9999999999999999999999))); // change this before deploying if needed
+        console.log("Deployer: ", txOrigin);
+        console.log("Create2 Deployer: ", create2Deployer);
+        console.logBytes32(salt);
+
+        address computedAddress = vm.computeCreate2Address(
+            salt,
+            keccak256(abi.encodePacked(type(BungeeApproveAndBridge).creationCode, abi.encode(SOCKET_GATEWAY))),
+            create2Deployer
+        );
+        console.log("Computed address: ", computedAddress);
 
         bungeeApproveAndBridge = BungeeApproveAndBridge(deploy(salt));
         console.log("Deployed BungeeApproveAndBridge at: ", address(bungeeApproveAndBridge));
     }
 
-    function deploy(uint256 salt) public returns (address addr) {
+    function deploy(bytes32 salt) public returns (address addr) {
         vm.broadcast();
-        BungeeApproveAndBridge _bungeeApproveAndBridge = new BungeeApproveAndBridge{salt: bytes32(salt)}(SOCKET_GATEWAY);
+        BungeeApproveAndBridge _bungeeApproveAndBridge = new BungeeApproveAndBridge{salt: salt}(SOCKET_GATEWAY);
         addr = address(_bungeeApproveAndBridge);
         console.log("Deployed contract to: %s", address(addr));
 
