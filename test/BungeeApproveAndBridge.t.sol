f88002441ae62a06c5344625c92af47fdeb20a08
Sebastian T F <sebastian.tharakan97@gmail.com>
2025-07-21 17:34:38 +0530

    test: applyPctDiff _compare == _base

--- test/BungeeApproveAndBridge.t.sol
+++ test/BungeeApproveAndBridge.t.sol
@@ -5,16 +5,33 @@ import {Test} from "forge-std/Test.sol";
 
 import {BungeeApproveAndBridge} from "src/BungeeApproveAndBridge.sol";
 
+contract PublicBungeeApproveAndBridge is BungeeApproveAndBridge {
+    constructor(address _socketGateway) BungeeApproveAndBridge(_socketGateway) {}
+
+    function applyPctDiff(uint256 _base, uint256 _compare, uint256 _target) public view returns (uint256) {
+        return super._applyPctDiff(_base, _compare, _target);
+    }
+}
+
 contract BungeeApproveAndBridgeTest is Test {
-    BungeeApproveAndBridge public bungeeApproveAndBridge;
+    PublicBungeeApproveAndBridge public bungeeApproveAndBridge;
 
     address constant SOCKET_GATEWAY = 0x3a23F943181408EAC424116Af7b7790c94Cb97a5;
 
     function setUp() public {
-        bungeeApproveAndBridge = new BungeeApproveAndBridge(SOCKET_GATEWAY);
+        bungeeApproveAndBridge = new PublicBungeeApproveAndBridge(SOCKET_GATEWAY);
     }
 
     function test_constructor() public {
         assertEq(bungeeApproveAndBridge.SOCKET_GATEWAY(), SOCKET_GATEWAY);
+    }
+
+    function test_applyPctDiff() public {
+        assertEq(bungeeApproveAndBridge.applyPctDiff({_base: 100, _compare: 110, _target: 100}), 110);
+        assertEq(bungeeApproveAndBridge.applyPctDiff({_base: 100, _compare: 90, _target: 100}), 90);
+    }
+
+    function test_applyPctDiff_equal() public {
+        assertEq(bungeeApproveAndBridge.applyPctDiff({_base: 100, _compare: 100, _target: 100}), 100);
     }
 }
